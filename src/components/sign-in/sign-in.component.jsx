3543cde809b0b9831ea85e88c51865636a088554
Longzhen <shame.to.say1931@gmail.com>
2021-07-03 17:00:09 -0500

    add sign in with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -16,10 +16,16 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
-    }
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        }catch(error){
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
@@ -38,7 +44,7 @@ class SignIn extends React.Component {
                         type='email'
                         handleChange={this.handleChange}
                         value={this.state.email}
-                        label='email'
+                        label='Email'
                         required
                     />
 
@@ -47,7 +53,7 @@ class SignIn extends React.Component {
                         type='password'
                         value={this.state.password}
                         handleChange={this.handleChange}
-                        label='password'
+                        label='Password'
                         required
                     />
                     <div className='buttons'>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,39 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyD2ntBjA4qLnDwit52O1_5vW_RygiTIDyk",
-    authDomain: "cloth-db-97c70.firebaseapp.com",
-    projectId: "cloth-db-97c70",
-    storageBucket: "cloth-db-97c70.appspot.com",
-    messagingSenderId: "574835751886",
-    appId: "1:574835751886:web:2381c7569871f6ccb6c99f",
-    measurementId: "G-KQW4KVQ3Z5"
-  };
+  apiKey: "AIzaSyD2ntBjA4qLnDwit52O1_5vW_RygiTIDyk",
+  authDomain: "cloth-db-97c70.firebaseapp.com",
+  projectId: "cloth-db-97c70",
+  storageBucket: "cloth-db-97c70.appspot.com",
+  messagingSenderId: "574835751886",
+  appId: "1:574835751886:web:2381c7569871f6ccb6c99f",
+  measurementId: "G-KQW4KVQ3Z5"
+};
+
+export const createUserProfileDocument = async(userAuth, additionalData)=>{
+  if(!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+  const snapShot = await userRef.get();
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    
+    const createdAt = new Date();
+    
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }catch (error){
+        console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
 
 firebase.initializeApp(config);
 
@@ -18,7 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
